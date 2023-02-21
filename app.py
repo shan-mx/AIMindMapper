baf31b4bccfe6685e9737f5061776a366ace616f
Alex Shan <36291011+shan-mx@users.noreply.github.com>
2023-02-21 22:58:58 +0800

    fix: keys

--- app.py
+++ app.py
@@ -5,20 +5,27 @@ import xmind
 from helper import *
 from vars import *
 
-openai.api_key = api_key
+openai.api_key = api_key1 + api_key2 + api_key3
 st.set_page_config(page_title='AI MindMapper', page_icon='🤖')
 st.title('AI MindMapper')
 topic = st.text_area('Topic or Text', '')
 gen_button = st.button('Generate')
-if gen_button:
+if gen_button and topic != "":
     with st.spinner("Please wait..."):
-        d = eval(openai.Completion.create(model="text-davinci-003", prompt=instruction_prompt + topic + "\n", max_tokens=3000, temperature=0.3)["choices"][0]["text"])
+        nothing_in_it = {
+            "model": "text-davinci-003",
+            "prompt": instruction_prompt + topic + "\n",
+            "max_tokens": 3000,
+            "temperature": 0.3
+        }
+        d = openai.Completion.create(**nothing_in_it)
         print(d)
-        #except:
-        #    st.error("There's an error accessing the GPT model, please try again.")
-        #    exit(0)
+        try:
+            d = eval(d["choices"][0]["text"])
+        except TypeError:
+            st.error("Disconnected from OpenAi, fix your network")
+            exit(0)
         topic = d["title"]
-        st.markdown(generate_markdown(d))
         workbook = xmind.load(topic + ".xmind")
         sheet = workbook.getPrimarySheet()
         sheet.setTitle(topic)
@@ -30,3 +37,4 @@ if gen_button:
         with open(topic + '.xmind', 'rb') as f:
             st.download_button('Download XMind File (for XMind 8)', f, file_name=topic + '.xmind')
         os.remove(topic + '.xmind')
+        st.markdown(generate_markdown(d))
--- vars.py
+++ vars.py
@@ -1,2 +1,4 @@
-api_key = "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"
+api_key1 = "sk-UIBijhqxo2md"
+api_key2 = "z2NozTAJT3BlbkF"
+api_key3 = "Jkyp6t1Cr9F1EdKPxwCid"
 instruction_prompt = 'generate a large-scale complex multi-level mind map (in JSON format, "title"+"children") with specific information in a professional perspective(note: try to put new knowledge to expand/explain the terms, e.g definitions, examples) for the following text:\n'
