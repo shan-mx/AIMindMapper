429c86086a8d9a5a626d1e286e912cff4d6578c0
Alex Shan <36291011+shan-mx@users.noreply.github.com>
2023-11-01 18:37:21 +0800

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,3 +1,3 @@
-api_key1 = "sk-UIBijhqxo2mdz2NozTA"
-api_key2 = "JT3BlbkFJkyp6t1Cr9F1EdKPxwCid"
+api_key1 = "sk-"
+api_key2 = ""
 instruction_prompt = 'generate a large-scale complex multi-level mind map (in JSON format, "title"+"children") with specific information in a professional perspective(note: try to put new knowledge to expand/explain the terms, e.g definitions, examples) for the following text:'
